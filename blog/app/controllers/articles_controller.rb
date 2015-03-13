6536f69d33b2915c31317dcf9eaa9049bfd3b817
WENHAO MA <marvenhool911@hotmail.com>
2015-03-13 17:33:10 +0900

    css file 合併

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
 
   #http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
+  layout 'articles'
+
   def index
     @articles = Article.all
     render ('articles/index')
