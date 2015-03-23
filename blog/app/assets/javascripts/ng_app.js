d81d612735239d9c71e0372bb0c86b5c53957811
WENHAO MA <marvenhool911@hotmail.com>
2015-03-23 15:11:22 +0900

    layout変更

--- blog/app/assets/javascripts/ng_app.js
+++ blog/app/assets/javascripts/ng_app.js
@@ -1,22 +1,22 @@
-///**
-// * Created by hikari-user on 2015/03/20.
-// */
-//(function() {
-//    var app;
-//
-//    app = angular.module('sampleApp', []);
-//
-//    app.config(function($httpProvider) {
-//        var authToken;
-//        authToken = $("meta[name=\"csrf-token\"]").attr("content");
-//        return $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
-//    });
-//
-//    $(document).on('page:load', function() {
-//        return $('[ng-app]').each(function() {
-//            var module;
-//            module = $(this).attr('ng-app');
-//            return angular.bootstrap(this, [module]);
-//        });
-//    });
-//}).call(this);
+/**
+* Created by hikari-user on 2015/03/20.
+*/
+(function() {
+    var app;
+
+    app = angular.module('appRoot', []);
+
+    app.config(function($httpProvider) {
+        var authToken;
+        authToken = $("meta[name=\"csrf-token\"]").attr("content");
+        return $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+    });
+
+    $(document).on('page:load', function() {
+        return $('[ng-app]').each(function() {
+            var module;
+            module = $(this).attr('ng-app');
+            return angular.bootstrap(this, [module]);
+        });
+    });
+}).call(this);
